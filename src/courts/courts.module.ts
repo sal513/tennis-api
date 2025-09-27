112e75257836c38381aad51ef70acc0a9fc59278
Salim Kisma <salimkisma13@gmail.com>
2025-09-27 21:49:17 +0200

    feat(courts): REST controller + wiring module/app + validation

--- src/courts/courts.module.ts
+++ src/courts/courts.module.ts
@@ -1,10 +1,5 @@
 import { Module } from '@nestjs/common';
 import { CourtsController } from './courts.controller';
 import { CourtsService } from './courts.service';
-import { CourtsService } from './courts.service';
-
-@Module({
-  controllers: [CourtsController],
-  providers: [CourtsService]
-})
+@Module({ controllers: [CourtsController], providers: [CourtsService] })
 export class CourtsModule {}
